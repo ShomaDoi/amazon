695f99fecdf481f7ed86b747f315d68d9a70ff0e
Sebastian <sebastiangenorth@gmail.com>
2024-03-19 12:55:43 +0100

    added a new list with delivery options, made the deliver options up to date

--- data/cart.js
+++ data/cart.js
@@ -3,7 +3,8 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
     cart = [{
         productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1'
     }];
 }
 
@@ -26,7 +27,8 @@ export function addToCart(productId) {
               }else{
                   cart.push({
                       productId: productId,
-                      quantity: 1
+                      quantity: 1,
+                      deliveryOptionId: '1'
                   });
               }
 
