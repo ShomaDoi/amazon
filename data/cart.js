7e279aa3ea9930c8689e4d835731ba171f5a6aad
Sebastian <sebastiangenorth@gmail.com>
2024-03-25 14:48:04 +0100

    reogranazied the project structure, implemented more tests

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,18 @@
 
 
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [{
-        productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: '1'
-    }];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    if (!cart) {
+        cart = [{
+            productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: '1'
+        }];
+    }
 }
 
 function saveToStorage() {
@@ -41,7 +46,6 @@ export function removeFromCart(productId) {
     
     const newCart = cart.filter(cartItem => cartItem.productId !== productId);
     cart.length = 0;
-    console.log(newCart);
     newCart.forEach(cartItem => cart.push(cartItem));
     saveToStorage();
    
